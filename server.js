5288f65222bbd117a939b00f5a619b0829de2f6d
Hang858 <984619420@qq.com>
2026-06-06 12:25:59 +0800

    适配部署

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(join(__dirname, 'dist')));
+
+app.get('*', (_, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
